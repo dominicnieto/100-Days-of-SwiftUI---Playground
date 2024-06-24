b7543fe2b80311435fad82fe2b5847857225e6c5
dominicnieto <dominicnieto27@gmail.com>
2024-06-24 14:01:07 -0600

    Day 2!

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,26 @@
 import SwiftUI
 
-var greeting = "Hello, playground"
-print(greeting)
-greeting = "Hi"
-print(greeting)
+var strings: [String] = ["Hello", "World", "Hello"]
 
-let name = "Dominic"
-print(name)
+var count = strings.count
+print(count)
 
+var unquieItems = Set(strings)
+print(unquieItems)
+
+var unquieCount = unquieItems.count
+print(unquieCount)
+
+var score = 80
+
+if score > 70 {
+    print(score)
+}
+let speed = 80
+let percentage = 85
+let age = 18
+
+if speed >= 80 {
+    print("Youre going too fast!")
+}
 
