9358331618513a883ba4ade5f954604db29af5c5
dominicnieto <dominicnieto27@gmail.com>
2024-06-26 15:47:07 -0600

    5th Day!

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -20,3 +20,21 @@ if transport == transportation.airplane || transport == transportation.helicopte
 } else {
     print("Have a nice drive!")
 }
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+switch forecast {
+case .sun:
+    print("It is hot!")
+case .rain:
+    print("Nice! Smells good!")
+case .wind:
+    print("Wear a jacket!")
+case .snow:
+    print("School is cancelled!")
+case .unknown:
+    print("The forcast is broken!")
+}
