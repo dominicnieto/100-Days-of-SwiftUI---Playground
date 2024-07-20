a75a696b0cdbc4d202f9a6e6509e6ae23fd92ded
dominicnieto <dominicnieto27@gmail.com>
2024-07-20 11:04:25 -0600

    Day 5

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,13 @@
 import SwiftUI
 
-var age = 18
-
-if age >= 18 {
-    print("You are young!")
-} else {
-    print("You are old!")
-}
-
-enum transportation {
-    case airplane, helicopter, car
-}
-
-let transport = transportation.airplane
-
-
-if transport == transportation.airplane || transport == transportation.helicopter {
-    print("Let's fly!")
-} else {
-    print("Have a nice drive!")
-}
-enum Weather {
-    case sun, rain, wind, snow, unknown
-}
-
-let forecast = Weather.sun
-
-switch forecast {
-case .sun:
-    print("It is hot!")
-case .rain:
-    print("Nice! Smells good!")
-case .wind:
-    print("Wear a jacket!")
-case .snow:
-    print("School is cancelled!")
-case .unknown:
-    print("The forcast is broken!")
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else if i.isMultiple(of: 3) {
+        print("FizzBuzz")
+    } else {
+        print(i)
+    }
 }
