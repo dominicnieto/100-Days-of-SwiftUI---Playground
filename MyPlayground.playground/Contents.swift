7ab4c8af88a0c5a0156f4b9f4541f57d818a1d5a
dominicnieto <dominicnieto27@gmail.com>
2024-08-01 20:07:44 -0600

    day 7

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,38 @@
 import SwiftUI
 
-for i in 1...100 {
-    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
-        print("Fizz")
-    } else if i.isMultiple(of: 5) {
-        print("Buzz")
-    } else if i.isMultiple(of: 3) {
-        print("FizzBuzz")
-    } else {
-        print(i)
+enum RootError: Error {
+    case tooLow, tooHigh, notFound
+}
+
+func squareRoot(of number: Int) throws -> Int {
+    if number < 1 {
+        throw RootError.tooLow
     }
+    
+    if number > 10_000 {
+        throw RootError.tooHigh
+    }
+    
+    // More code
+    
+    for i in 1...100 {
+        if i * i == number {
+            return i
+        }
+    }
+    
+    throw RootError.notFound
+}
+
+let number = 460
+
+do {
+    let root = try squareRoot(of: number)
+    print("The square root of \(number) is \(root)!")
+} catch RootError.tooLow {
+    print("Sorry choose a larger number!")
+} catch RootError.tooHigh {
+    print("Sorry choose a smaller number!")
+} catch RootError.notFound {
+    print("This number does not have square root!")
 }
