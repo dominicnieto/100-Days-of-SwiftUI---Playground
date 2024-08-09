2532e49547d1273386b0fecaa2b8f4eae56870c2
dominicnieto <dominicnieto27@gmail.com>
2024-08-08 22:38:32 -0600

    day 11

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,2 +1,30 @@
 import SwiftUI
 
+struct Car {
+    let model: String
+    let seats: Int
+    let maximumGears: Int
+    private(set) var gear = 1
+    
+    init(model: String, seats: Int, maximumGears: Int, gear: Int = 1) {
+        self.model = model
+        self.seats = seats
+        self.maximumGears = maximumGears
+        self.gear = gear
+    }
+    
+    mutating func changeGear(to difference: Int) {
+        let newGear = gear + difference
+        
+        if gear < 1 {
+            gear = 1
+        } else if gear > maximumGears {
+            gear = maximumGears
+        }
+    }
+}
+
+var car = Car(model: "Tesla", seats: 6, maximumGears: 8)
+car.changeGear(to: 1)
+print(car.gear)
+
